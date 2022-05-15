ed722dc240f10c63a39d431f567d190489110f74
Pete Fowler <fowler.pete@gmail.com>
2022-05-15 17:38:54 -0600

    Create outer container

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+make a 16x16 grid of divs using javascript
+    document.addElement to create 16 divs within an outer container div
+
+    CSS to style it into a grid
+*/
+
+// Outer container
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.classList = "container";
+

9922ddea975bc19bb8c2f7995f8b7bb41f2552cc
Pete Fowler <fowler.pete@gmail.com>
2022-05-15 20:05:15 -0600

    Finish 16 x 16 with hover effect

--- script.js
+++ script.js
@@ -6,16 +6,28 @@ make a 16x16 grid of divs using javascript
 */
 
 // Outer container
-const container = document.createElement('div');
-container.className = "container";
-document.body.appendChild(container);
+    const container = document.createElement('div');
+    container.className = "container";
+    document.body.appendChild(container);
 
 
-// 16 x 16 grid of divs
-for (let i = 0; i < 255; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.className = "grid-div";
-    container.appendChild(gridDiv);
-}
+// Create inital 16 x 16 grid of divs
+    for (let i = 0; i < 255; i++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.className = "grid-div";
+        container.appendChild(gridDiv);
+    }
 
+// Add hover effect to boxes
+    const boxes = document.querySelectorAll('.grid-div');
+    boxes.forEach(hoverEffect);
+
+    function hoverEffect (box) {
+        box.addEventListener('mouseenter', changeColor);
+    }
+
+    function changeColor() {
+        this.setAttribute('style', 'color: blue; background-color: blue;');
+
+    }
 

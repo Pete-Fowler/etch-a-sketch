193ffab0fb3f6fbea503cff25d739eaa16e3743e
Pete Fowler <fowler.pete@gmail.com>
2022-05-16 19:09:31 -0600

    Convert grid code into makeGrid()

--- script.js
+++ script.js
@@ -6,35 +6,49 @@ make a 16x16 grid of divs using javascript
 */
 
 // Grid size slider
-    const slider = document.querySelector('.slider');
-    const sliderTextBox = document.querySelector('.slider-text-box');
-    sliderTextBox.textContent = `${slider.value} x ${slider.value}`;
-    slider.oninput = () => sliderTextBox.textContent = `${slider.value} x 
-    ${slider.value}`;
+const slider = document.querySelector('.slider');
+const sliderTextBox = document.querySelector('.slider-text-box');
+sliderTextBox.textContent = `Grid size: ${slider.value} x ${slider.value}`;
+slider.oninput = () => sliderTextBox.textContent = `Grid size: ${slider.value} x 
+${slider.value}`;
 
-// Outer grid container
+function makeGrid(length = 16) {
+    if (document.querySelector('.container')) {
+    body.removeChild(container);
+    }
+
+    // Outer grid container
     const container = document.createElement('div');
     container.className = "container";
     document.body.appendChild(container);
 
-
-// Create inital 16 x 16 grid of divs
-    for (let i = 0; i < 255; i++) {
+    // Create grid of divs    
+    for (let i = 0; i < length * length; i++) {
         const gridDiv = document.createElement('div');
         gridDiv.className = "grid-div";
         container.appendChild(gridDiv);
-    }
+        }
+    
+    // Event listener for grid size input
+    
+}
+
+makeGrid(slider.value);
+
+
+ 
+
 
 // Add hover effect to boxes
-    const boxes = document.querySelectorAll('.grid-div');
-    boxes.forEach(hoverEffect);
+const boxes = document.querySelectorAll('.grid-div');
+boxes.forEach(hoverEffect);
 
-    function hoverEffect (box) {
-        box.addEventListener('mouseenter', changeColor);
-    }
+function hoverEffect (box) {
+    box.addEventListener('mouseenter', changeColor);
+}
 
-    function changeColor() {
-        this.setAttribute('style', 'background-color: blue;');
+function changeColor() {
+    this.setAttribute('style', 'background-color: blue;');
 
-    }
+}
 
